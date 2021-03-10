c2cd46858bc5778a5eb172d2e6fcf9045e21e0d9
Corrinne W <corrinneworden@gmail.com>
2021-03-10 18:50:51 -0500

    worked on persisting checked state in grocery list

--- assets/js/grocery-list-script.js
+++ assets/js/grocery-list-script.js
@@ -20,34 +20,52 @@ let checklistItemText;
 //initialize localStorage for pantryItems
 const pantryArr = JSON.parse(localStorage.getItem('pantryItems')) || [];
 
+// grab the user selected recipes
+const userSelectedRecipes = JSON.parse(localStorage.getItem('userSelectedRecipes'));
+
 //initialize localStorage for li
 const listItems = JSON.parse(localStorage.getItem('li')) || [];
 
+//initialize localStorage for checkBoxes (to persist checkbox state)
+const checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {};
+
 //build checklistItems for each missedIngredient
 jQuery.each(pantryArr, function (index, value) {
-    //pull missedIngredients from pantryItems (spoonacular api data)
-    const missedIngredients = pantryArr[index].missedIngredients;
+    // this only allows missing ingredients which are from recipes which the user selected are added
+    jQuery.each(userSelectedRecipes, function(i, v){
+        if(pantryArr[index].id === Number.parseInt(userSelectedRecipes[0])) {
+            //pull missedIngredients from pantryItems (spoonacular api data)
+            const missedIngredients = pantryArr[index].missedIngredients;
+            jQuery.each(missedIngredients, function(index, value) {
+                let li = missedIngredients[index].name;
+                listItems.push(li);
+                localStorage.setItem("li", JSON.stringify(listItems));
+            })
+    
+            //create DOM elements & append to listContainer
+            checkBox = $("<input/>")
+                .attr('type', 'checkbox', 'id', 'flexCheckDefault')
+                .addClass('form-check-input')
+                .css('padding-right', '10px');
+    
+            jQuery.each(listItems, function (index, value) {
+                checklistItemText = $("<label></label>")
+                .addClass('form-check-label')
+                .text(listItems[index])
+            })
 
-    jQuery.each(missedIngredients, function(index, value) {
-        let li = missedIngredients[index].name;
-        listItems.push(li);
-        localStorage.setItem("li", JSON.stringify(listItems));
+            checkBox.on('change', function() {
+                checkBox.each(function() {
+                    checkboxValues[this] = this.checked;
+                    console.log(this.checked)
+                })
+                localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+            })
+    
+            checklistItem = $("<div><div/>").append(checkBox, checklistItemText);
+            listContainer.append(checklistItem);
+        }
     })
-
-    //create DOM elements & append to listContainer
-    checkBox = $("<input/>")
-        .attr('type', 'checkbox', 'id', 'flexCheckDefault')
-        .addClass('form-check-input')
-        .css('padding-right', '10px');
-
-    jQuery.each(listItems, function (index, value) {
-        checklistItemText = $("<label></label>")
-        .addClass('form-check-label')
-        .text(listItems[index])
-    })
-
-    checklistItem = $("<div><div/>").append(checkBox, checklistItemText);
-    listContainer.append(checklistItem);
 })
 
 //TODO: when item is checked, persist checked state in localStorage
