3ab435e4b6eba787380a111b398a3a1f3f565f82
Keon Pourboghrat <kekons4@gmail.com>
2021-03-12 14:26:03 -0500

    added async function to loading missing ingedients in grocery html page

--- assets/js/recipe-results-script.js
+++ assets/js/recipe-results-script.js
@@ -50,103 +50,103 @@ $(function () {
 
         //append card to container
         recipesContainer.append(card);
-        fetch("https://api.spoonacular.com/recipes/" + recipe.id + "/information?includeNutrition=true&apiKey=3bf0573f77794a1fbc310965fd52f563")
-            .then(function (response) {
-                if (response.ok) {
-                    response.json().then(function (data) {
-                        nutritionItems.push(data);
-                    });
-                }
-            })
+        // fetch("https://api.spoonacular.com/recipes/" + recipe.id + "/information?includeNutrition=true&apiKey=3bf0573f77794a1fbc310965fd52f563")
+        //     .then(function (response) {
+        //         if (response.ok) {
+        //             response.json().then(function (data) {
+        //                 nutritionItems.push(data);
+        //             });
+        //         }
+        //     })
     });
 });
 
 // Resets the recipe container to show all recipes
-function resetDom() {
-    recipesContainer.empty();
-    pantryArr.results.forEach(recipe => {
-        // create card object
-        let card = $("<div></div>");
-        card.addClass("recipe-card");
-        card.attr("data-id", recipe.id);
-        // creates title for card
-        let cardTitle = $("<p></p>");
-        cardTitle.text(recipe.title);
-        let cardImg = $("<img>");
-        cardImg.attr("src", recipe.image);
+// function resetDom() {
+//     recipesContainer.empty();
+//     pantryArr.results.forEach(recipe => {
+//         // create card object
+//         let card = $("<div></div>");
+//         card.addClass("recipe-card");
+//         card.attr("data-id", recipe.id);
+//         // creates title for card
+//         let cardTitle = $("<p></p>");
+//         cardTitle.text(recipe.title);
+//         let cardImg = $("<img>");
+//         cardImg.attr("src", recipe.image);
 
-        //append title and image to card
-        card.append(cardTitle);
-        card.append(cardImg);
+//         //append title and image to card
+//         card.append(cardTitle);
+//         card.append(cardImg);
 
-        //append card to container
-        recipesContainer.append(card);
-    });
-}
+//         //append card to container
+//         recipesContainer.append(card);
+//     });
+// }
 
 // display filtered list ot DOM
-function displayFilteredRecipes() {
-    // console.log(nutritionArr);
-    // Empty the recipes container for recipes
-    recipesContainer.empty();
+// function displayFilteredRecipes() {
+//     // console.log(nutritionArr);
+//     // Empty the recipes container for recipes
+//     recipesContainer.empty();
 
-    // go through the pantryArr again
-    pantryArr.results.forEach(recipe => {
-        // check to see if the pantryArr elements have the same id's as the ones in the filtered list
-        nutritionArr.forEach(id => {
-            if (id === recipe.id) {
-                // create card object
-                let card = $("<div></div>");
-                card.addClass("recipe-card");
-                card.attr("data-id", recipe.id);
-                // creates title for card
-                let cardTitle = $("<p></p>");
-                cardTitle.text(recipe.title);
-                let cardImg = $("<img>");
-                cardImg.attr("src", recipe.image);
+//     // go through the pantryArr again
+//     pantryArr.results.forEach(recipe => {
+//         // check to see if the pantryArr elements have the same id's as the ones in the filtered list
+//         nutritionArr.forEach(id => {
+//             if (id === recipe.id) {
+//                 // create card object
+//                 let card = $("<div></div>");
+//                 card.addClass("recipe-card");
+//                 card.attr("data-id", recipe.id);
+//                 // creates title for card
+//                 let cardTitle = $("<p></p>");
+//                 cardTitle.text(recipe.title);
+//                 let cardImg = $("<img>");
+//                 cardImg.attr("src", recipe.image);
 
-                //append title and image to card
-                card.append(cardTitle);
-                card.append(cardImg);
+//                 //append title and image to card
+//                 card.append(cardTitle);
+//                 card.append(cardImg);
 
-                //append card to container
-                recipesContainer.append(card);
-            }
-        });
-    });
-}
+//                 //append card to container
+//                 recipesContainer.append(card);
+//             }
+//         });
+//     });
+// }
 
 // grab api data on nutrition of each recipes, filter out based on recipe id
 // and store the recipes id based on vegan, vegetarian, glutenFree, or dairyFree
-function filteredRecipes(recipeIds, filter) {
-    // recipeIds.forEach(id => {
-    for (let i = 0; i < nutritionItems.length; i++) {
-        // if vegan push id
-        if (filter === "vegan") {
-            if (nutritionItems[i].vegan === true) {
-                nutritionArr.push(nutritionItems[i].id);
-            }
-            // if vegetarian push id
-        } else if (filter === "vegetarian") {
-            if (nutritionItems[i].vegetarian === true) {
-                nutritionArr.push(nutritionItems[i].id);
-            }
-            // if gluten free push id
-        } else if (filter === "glutenFree") {
-            if (nutritionItems[i].glutenFree === true) {
-                nutritionArr.push(nutritionItems[i].id);
-            }
-            // if dairy free push id
-        } else if (filter === "dairyFree") {
-            if (nutritionItems[i].dairyFree === true) {
-                nutritionArr.push(nutritionItems[i].id);
-            }
-        }
-    }
-    // });
-    //constantly update the DOM with new recipes
-    displayFilteredRecipes();
-}
+// function filteredRecipes(recipeIds, filter) {
+//     // recipeIds.forEach(id => {
+//     for (let i = 0; i < nutritionItems.length; i++) {
+//         // if vegan push id
+//         if (filter === "vegan") {
+//             if (nutritionItems[i].vegan === true) {
+//                 nutritionArr.push(nutritionItems[i].id);
+//             }
+//             // if vegetarian push id
+//         } else if (filter === "vegetarian") {
+//             if (nutritionItems[i].vegetarian === true) {
+//                 nutritionArr.push(nutritionItems[i].id);
+//             }
+//             // if gluten free push id
+//         } else if (filter === "glutenFree") {
+//             if (nutritionItems[i].glutenFree === true) {
+//                 nutritionArr.push(nutritionItems[i].id);
+//             }
+//             // if dairy free push id
+//         } else if (filter === "dairyFree") {
+//             if (nutritionItems[i].dairyFree === true) {
+//                 nutritionArr.push(nutritionItems[i].id);
+//             }
+//         }
+//     }
+//     // });
+//     //constantly update the DOM with new recipes
+//     displayFilteredRecipes();
+// }
 
 //upon clicking on one of the recipe cards, it will add its id to an array called userSelected
 //this will be used later for displaying alot of other information
